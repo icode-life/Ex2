e59c86f6452503005c7e0ba71c3d677935db28fd
Vincenzo Sanfratello <vincenzo.sanfratello@akabi.eu>
2023-09-03 14:33:02 +0200

    Add Enum UserChoice and change logic to Exit Program

--- VS_Console_Boulangerie_2/Program.cs
+++ VS_Console_Boulangerie_2/Program.cs
@@ -2,6 +2,15 @@
 {
     public class Program
     {
+        private enum UserChoice
+        {
+            SELL_BAGUETTE = 1,
+            SELL_BREAD = 2,
+            GET_CASH_AMOUNT = 3,
+            QUIT = 4,
+            UNASSIGNED
+        }
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -12,36 +21,38 @@
             Bakery bakery = new Bakery();
             Console.WriteLine("Bonjour,\nBienvenue dans l'application Backery mgmt app!");
 
+            UserChoice userChoice = UserChoice.UNASSIGNED;
+
             //démarrage de la boucle
             do
             {
                 Console.WriteLine("Veuillez tapez le numéro de votre choix:\n1) vente: baguette\n2) vente: pain\n3) consulter le produit des ventes\n4) exit");
                 userPick = Console.ReadLine();
-
-                //
-                switch (userPick)
+                if (Enum.TryParse(userPick, out userChoice))
                 {
-                    case "1":
-                        bakery.SellBaguette();
-                        break;
+                    switch (userChoice)
+                    {
+                        case UserChoice.SELL_BAGUETTE:
+                            bakery.SellBaguette();
+                            break;
 
-                    case "2":
-                        bakery.SellBread();
-                        break;
+                        case UserChoice.SELL_BREAD:
+                            bakery.SellBread();
+                            break;
 
-                    case "3":
-                        Console.WriteLine($"Le total actuel des ventes s'élève à {Math.Round(bakery.GetCashRegisterAmount(), 2)} €\n\n");
-                        break;
+                        case UserChoice.GET_CASH_AMOUNT:
+                            Console.WriteLine($"Le total actuel des ventes s'élève à {Math.Round(bakery.GetCashRegisterAmount(), 2)} €\n\n");
+                            break;
 
-                    default:
-                        break;
+                        case UserChoice.QUIT:
+                            Console.WriteLine("Merci d'avoir utilisé BakeryMgmtApp!\nNous vous souhaitons une excellente journée!");
+                            //ajout d'un readLine pour laisser le temps de lecture avant exit
+                            Console.ReadLine();
+                            Environment.Exit(1);
+                            break;
+                    }
                 }
-            } while (userPick != "4");
-
-            Console.WriteLine("Merci d'avoir utilisé BakeryMgmtApp!\nNous vous souhaitons une excellente journée!");
-
-            //ajout d'un readLine pour laisser le temps de lecture avant exit
-            Console.ReadLine();
+            } while (true);
         }
     }
 }
